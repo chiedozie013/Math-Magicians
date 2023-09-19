56acbcc1ccbb3e34d07e613c80efdfe47a8da4c9
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-09-19 19:30:03 +0100

    update on project

---
+++ src/components/Calculator.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import CalculatorDisplay from './CalculatorDisplay';
+
+const Calculator = () => (
+  <>
+    <CalculatorDisplay />
+  </>
+);
+export default Calculator;
---
+++ src/components/CalculatorDisplay.js
@@ -0,0 +1,146 @@
+import React from 'react';
+
+const CalculatorDisplay = () => (
+  <>
+    {/* <div className="container">
+      <div>
+        <p>0</p>
+      </div>
+      <div>
+        <button type="submit" className="white">
+          AC
+        </button>
+        <button type="submit" className="white">
+          +/-
+        </button>
+        <button type="submit" className="white">
+          %
+        </button>
+        <button type="submit" className="white">
+          +
+        </button>
+      </div>
+      <div>
+        <button type="submit" className="white">
+          7
+        </button>
+        <button type="submit" className="white">
+          8
+        </button>
+        <button type="submit" className="white">
+          9
+        </button>
+        <button type="submit" className="white">
+          *
+        </button>
+      </div>
+      <div>
+        <button type="submit" className="white">
+          4
+        </button>
+        <button type="submit" className="white">
+          5
+        </button>
+        <button type="submit" className="white">
+          6
+        </button>
+        <button type="submit" className="white">
+          -
+        </button>
+      </div>
+      <div>
+        <button type="submit" className="white">
+          1
+        </button>
+        <button type="submit" className="white">
+          2
+        </button>
+        <button type="submit" className="white">
+          3
+        </button>
+        <button type="submit" className="white">
+          +
+        </button>
+      </div>
+      <div>
+        <button type="submit" className="white">
+          0
+        </button>
+        <button type="submit" className="white">
+          .
+        </button>
+        <button type="submit" className="white">
+          =
+        </button>
+      </div>
+    </div> */}
+    <div className="container">
+      <div className="sum-display sum">
+        <span>0</span>
+      </div>
+
+      <button type="button" className="btn-white">
+        AC
+      </button>
+      <button type="button" className="btn-white">
+        +/-
+      </button>
+      <button type="button" className="btn-white">
+        %
+      </button>
+      <button type="button" className="btn-orange">
+        &#xf7;
+      </button>
+
+      <button type="button" className="btn-white">
+        7
+      </button>
+      <button type="button" className="btn-white">
+        8
+      </button>
+      <button type="button" className="btn-white">
+        9
+      </button>
+      <button type="button" className="btn-orange">
+        x
+      </button>
+
+      <button type="button" className="btn-white">
+        4
+      </button>
+      <button type="button" className="btn-white">
+        5
+      </button>
+      <button type="button" className="btn-white">
+        6
+      </button>
+      <button type="button" className="btn-orange">
+        -
+      </button>
+
+      <button type="button" className="btn-white">
+        1
+      </button>
+      <button type="button" className="btn-white">
+        2
+      </button>
+      <button type="button" className="btn-white">
+        3
+      </button>
+      <button type="button" className="btn-orange">
+        +
+      </button>
+
+      <button type="button" className="btn-white btn-zero">
+        0
+      </button>
+      <button type="button" className="btn-white">
+        .
+      </button>
+      <button type="button" className="btn-orange">
+        =
+      </button>
+    </div>
+  </>
+);
+export default CalculatorDisplay;

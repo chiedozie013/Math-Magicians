bfd28ba0e775f2cf105ece6c00409bcbc01ad8e6
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-09-20 09:38:40 +0100

    correct linters error

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,9 +3,9 @@ import CalculatorDisplay from './CalculatorDisplay';
 
 function Calculator() {
   return (
-  <div>
-    <CalculatorDisplay />
-  </div>
+    <div>
+      <CalculatorDisplay />
+    </div>
   );
 }
 export default Calculator;
--- src/components/CalculatorDisplay.js
+++ src/components/CalculatorDisplay.js
@@ -2,75 +2,75 @@ import React from 'react';
 
 function CalculatorDisplay() {
   return (
-  <div>
-    <div className="container">
-      <div className="sum-display sum">
-        <span>0</span>
+    <div>
+      <div className="container">
+        <div className="sum-display sum">
+          <span>0</span>
+        </div>
+
+        <button type="button" className="btn-white">
+          AC
+        </button>
+        <button type="button" className="btn-white">
+          +/-
+        </button>
+        <button type="button" className="btn-white">
+          %
+        </button>
+        <button type="button" className="btn-orange">
+          &#xf7;
+        </button>
+
+        <button type="button" className="btn-white">
+          7
+        </button>
+        <button type="button" className="btn-white">
+          8
+        </button>
+        <button type="button" className="btn-white">
+          9
+        </button>
+        <button type="button" className="btn-orange">
+          x
+        </button>
+
+        <button type="button" className="btn-white">
+          4
+        </button>
+        <button type="button" className="btn-white">
+          5
+        </button>
+        <button type="button" className="btn-white">
+          6
+        </button>
+        <button type="button" className="btn-orange">
+          -
+        </button>
+
+        <button type="button" className="btn-white">
+          1
+        </button>
+        <button type="button" className="btn-white">
+          2
+        </button>
+        <button type="button" className="btn-white">
+          3
+        </button>
+        <button type="button" className="btn-orange">
+          +
+        </button>
+
+        <button type="button" className="btn-white btn-zero">
+          0
+        </button>
+        <button type="button" className="btn-white">
+          .
+        </button>
+        <button type="button" className="btn-orange">
+          =
+        </button>
       </div>
-
-      <button type="button" className="btn-white">
-        AC
-      </button>
-      <button type="button" className="btn-white">
-        +/-
-      </button>
-      <button type="button" className="btn-white">
-        %
-      </button>
-      <button type="button" className="btn-orange">
-        &#xf7;
-      </button>
-
-      <button type="button" className="btn-white">
-        7
-      </button>
-      <button type="button" className="btn-white">
-        8
-      </button>
-      <button type="button" className="btn-white">
-        9
-      </button>
-      <button type="button" className="btn-orange">
-        x
-      </button>
-
-      <button type="button" className="btn-white">
-        4
-      </button>
-      <button type="button" className="btn-white">
-        5
-      </button>
-      <button type="button" className="btn-white">
-        6
-      </button>
-      <button type="button" className="btn-orange">
-        -
-      </button>
-
-      <button type="button" className="btn-white">
-        1
-      </button>
-      <button type="button" className="btn-white">
-        2
-      </button>
-      <button type="button" className="btn-white">
-        3
-      </button>
-      <button type="button" className="btn-orange">
-        +
-      </button>
-
-      <button type="button" className="btn-white btn-zero">
-        0
-      </button>
-      <button type="button" className="btn-white">
-        .
-      </button>
-      <button type="button" className="btn-orange">
-        =
-      </button>
     </div>
-  </div>
-);
+  );
 }
 export default CalculatorDisplay;
